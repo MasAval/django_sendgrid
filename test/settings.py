e5f83099b9b9584c8c67c9ff0cec75f9b7c30676
Alvaro Saavedra <asaavedra@masaval.cl>
2017-05-17 16:26:49 -0400

    add nose

--- test/settings.py
+++ test/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
     'local_sendgrid'
 )
 
@@ -105,3 +106,5 @@ STATIC_URL = '/static/'
 SENDGRID_API_KEY = (
     os.environ.get('SENDGRID_API_KEY')
 )
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
